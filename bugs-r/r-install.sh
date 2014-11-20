003d9b26edaeea05144e95fb9b19dd7f72e9b76f
Russell <russell@russelldunphy.com>
2014-11-20 14:09:24 +0000

    :poop: Add R dependencies

---
+++ bugs-r/r-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+while read line; do
+  package=$(echo $line | awk '{print $1}')
+  version=$(echo $line | awk '{print $2}')
+  tarball="R/lib/"$package"_"$version".tar.gz"
+  echo "Installing $package ($version)"
+  sudo R -e "install.packages(\"$tarball\", repo=NULL)"
+done < Rfile
