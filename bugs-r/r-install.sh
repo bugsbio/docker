88b52f53a4399964857f9659905e302d9800feaa
Russell <russell@russelldunphy.com>
2014-11-20 14:25:21 +0000

    :poop: Actually install r packages

--- bugs-r/r-install.sh
+++ bugs-r/r-install.sh
@@ -3,7 +3,7 @@ set -e
 while read line; do
   package=$(echo $line | awk '{print $1}')
   version=$(echo $line | awk '{print $2}')
-  tarball="R/lib/"$package"_"$version".tar.gz"
+  tarball="lib/"$package"_"$version".tar.gz"
   echo "Installing $package ($version)"
   sudo R -e "install.packages(\"$tarball\", repo=NULL)"
 done < Rfile
